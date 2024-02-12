c424221edf2c5beb7e23cd3c94b5932d5c678747
Oga-Emma <35017091+Oga-Emma@users.noreply.github.com>
2024-02-12 01:30:36 +0100

    Initial commit

---
+++ src/main/kotlin/app/seven/aventauth/AventauthApplication.kt
@@ -0,0 +1,11 @@
+package app.seven.aventauth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AventauthApplication
+
+fun main(args: Array<String>) {
+	runApplication<AventauthApplication>(*args)
+}
---
+++ src/test/kotlin/app/seven/aventauth/AventauthApplicationTests.kt
@@ -0,0 +1,13 @@
+package app.seven.aventauth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AventauthApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
